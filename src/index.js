a524a86e0dc0f8d79c4531f73a1fb419e744648b
nobu17 <lunacy171710@yahoo.co.jp>
2020-02-17 22:16:41 +0900

    change module name

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const amazonWishScraper = require('./amazonWishScraper');
+const amazonWishScraper = require('./amazonWishListScraper');
 
 async function main() {
   const itemList = await amazonWishScraper.getProductInfo('92QVMALK2QVW');
